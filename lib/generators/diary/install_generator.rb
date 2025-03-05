b5f71daf5c421258a7f37fa0db7340e45b065787
Despo Pentara <despo@extractmethod.com>
2025-03-05 14:56:37 +0000

    Move migrations to template directorys so they are managed correctly

--- lib/generators/diary/install_generator.rb
+++ lib/generators/diary/install_generator.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
+require "rails/generators"
+require "rails/generators/active_record"
 require 'rails/generators/base'
 
 module Diary
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __dir__)
+      include Rails::Generators::Migration
+
+      source_root File.expand_path('templates', __dir__)
 
       desc 'Creates a Diary initializer.'
 
       def copy_initializer
         template 'diary.rb', 'config/initializers/diary.rb'
+      end
+
+      def create_migrations
+        migration_template "create_diary_calendar_entries.rb", "db/migrate/create_diary_calendar_entries.rb"
+      end
+
+      def self.next_migration_number(dirname)
+        ActiveRecord::Generators::Base.next_migration_number(dirname)
       end
     end
   end
